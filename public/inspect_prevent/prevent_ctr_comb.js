6fb094c71ffff522a56a0eef55811d1b92037ccf
Navneet Kumar <92607923+anoxco0@users.noreply.github.com>
2022-02-24 00:10:52 +0530

    Update prevent_ctr_comb.js

--- public/inspect_prevent/prevent_ctr_comb.js
+++ public/inspect_prevent/prevent_ctr_comb.js
@@ -4,6 +4,7 @@ const fun = function(e) {
              e.keyCode === 86 || 
              e.keyCode === 85 || 
              e.keyCode === 117)) {
+                 alert("some")
             return false;
         } else {
             return true;
