f3686e0d0e44e6eed9ce0d45fa8bf6d9c4dfa057
Navneet Kumar <92607923+anoxco0@users.noreply.github.com>
2022-02-24 00:50:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 // require("dotenv").config();
-const express = require('express');
+// const express = require('express');
 // const connect = require('./configs/db');
 // const userController = require('./controllers/user.controller');
 // const {register, login} = require('./controllers/auth.controller');
 // const {body, validationResult} = require('express-validator');
 
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true }))
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true }))
 
-app.get('/', (req, res) => res.render('pages/index'))
-app.use('/register', userController);
+// app.get('/', (req, res) => res.render('pages/index'))
+// app.use('/register', userController);
 // app.use('/login', userController);
 // app.use('/users', userController);
 // app.post('/register', 
@@ -41,16 +41,16 @@ app.use('/register', userController);
 // body('password').isStrongPassword(), register);
 // app.post('/login', login)
 
-app.set("view engine", "ejs");
-app.use(express.static("public"))
+// app.set("view engine", "ejs");
+// app.use(express.static("public"))
 
-const port = process.env.PORT || 8222;
+// const port = process.env.PORT || 8222;
 
-app.listen(port, async(req, res)=>{
-    try{
-        // await connect();
-        console.log(`listening on port ${port} port....`)
-    } catch(e){
-        console.log({message:e.message});
-    }
-})
+// app.listen(port, async(req, res)=>{
+//     try{
+//         // await connect();
+//         console.log(`listening on port ${port} port....`)
+//     } catch(e){
+//         console.log({message:e.message});
+//     }
+// })
