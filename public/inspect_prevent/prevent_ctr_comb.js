37742f7f1f932185f71a4dff3b97c896aae09b46
Navneet Kumar <92607923+anoxco0@users.noreply.github.com>
2022-02-24 00:08:55 +0530

    hello

---
+++ public/inspect_prevent/prevent_ctr_comb.js
@@ -0,0 +1,12 @@
+const fun = function(e) {
+        if (e.ctrlKey && 
+            (e.keyCode === 67 || 
+             e.keyCode === 86 || 
+             e.keyCode === 85 || 
+             e.keyCode === 117)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+ export default fun;
