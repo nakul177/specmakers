03f93ae344933c45bcc3bf51cc4958e9012e63bf
Navneet Kumar <92607923+anoxco0@users.noreply.github.com>
2022-02-24 02:27:04 +0530

    Update index.js

--- public/js/users/index.js
+++ public/js/users/index.js
@@ -11,14 +11,14 @@ function onSubmitHandler(e) {
   // let email=document.getElementById("email").value;
   // let password=document.getElementById("password").value;
   
-//   function user(){
+  function user(){
 //     let name=document.getElementById("name").value;
 //     let email=document.getElementById("email").value;
 //     let password=document.getElementById("password").value;
 //     console.log(name.length)
 //     alert(name)
 //     // if(name.length>15 && name.length <3) alert ("name should be greater than 3 and smaller than 15 character")
-//   }
+  }
   fetch("http://localhost:8252/register?contentType=json")
     .then((response) => response.json())
     .then((data) => console.log(data));
